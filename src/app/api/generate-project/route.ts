01a10696ab17053734e4cdd06fb53aa67a5f000d
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2023-11-16 16:56:40 -0300

    feat(api): make route to handle with project creation

---
+++ src/app/api/generate-project/route.ts
@@ -0,0 +1,24 @@
+import { formSchema } from '@/components/form-settings/form'
+import { setNodeMetadata } from '@/scripts/set-node-metadata'
+import { setupScratch } from '@/scripts/setup-scratch'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+
+  const bodyParsed = formSchema.parse(body)
+
+  const metadata = {
+    name: bodyParsed.name,
+    author: bodyParsed.author,
+    description: bodyParsed.description,
+  }
+
+  const projectPath = setupScratch(bodyParsed)
+
+  setNodeMetadata(metadata, projectPath)
+
+  // TODO: [After that implement zip function] => fs.rmdirSync(projectPath)
+
+  return NextResponse.json({ message: 'ok' })
+}
