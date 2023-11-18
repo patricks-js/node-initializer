173f466aee5d847d9b83c14b45c8cc0c3f9bb457
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2023-11-17 21:22:23 -0300

    feat: make debounce function

---
+++ src/utils/debounce.ts
@@ -0,0 +1,9 @@
+export function debounce(func: (...args: any[]) => void, delay: number) {
+  let timeoutId: NodeJS.Timeout
+  return (...args: any[]) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
